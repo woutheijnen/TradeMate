ef5db987420674798708ce895e5010f011004c61
Wout Heijnen <woutheijnen@hotmail.com>
2018-11-25 23:07:12 +0100

    Initialize basic root components and set-up redux store + root reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+
+//Components
+import Home from "./components/Home";
+
+// Material UI
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+
+// Global CSS
 import "./App.css";
+
+const defaultTheme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Provider store={store}>
+        <MuiThemeProvider theme={defaultTheme}>
+          <div className="App">
+            <CssBaseline />
+            <Home />
+          </div>
+        </MuiThemeProvider>
+      </Provider>
+    );
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return <div className="Home">Test</div>;
+  }
+}
+
+export default Home;
