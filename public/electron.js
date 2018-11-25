1d5d154f6311f2d76ff4ce45ad2d84d2f30802f9
Wout Heijnen <woutheijnen@hotmail.com>
2018-11-25 18:26:32 +0100

    Configurate package.json, electron start and build & react with electron

---
+++ public/electron.js
@@ -0,0 +1,44 @@
+const electron = require("electron");
+const {
+  default: installExtension,
+  REDUX_DEVTOOLS
+} = require("electron-devtools-installer");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const url = require("url");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+installExtension(REDUX_DEVTOOLS)
+  .then(name => console.log(`Added Extension:  ${name}`))
+  .catch(err => console.log("An error occurred: ", err));
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ show: false });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.maximize();
+  mainWindow.show();
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
