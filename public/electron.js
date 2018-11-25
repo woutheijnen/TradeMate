b941f936dae83370048304fa07767ed0a16e9e18
Wout Heijnen <woutheijnen@hotmail.com>
2018-11-25 19:30:04 +0100

    Test : Fix electron-devtools-installer not found in production mode

--- public/electron.js
+++ public/electron.js
@@ -1,21 +1,21 @@
 const electron = require("electron");
-const {
-  default: installExtension,
-  REDUX_DEVTOOLS
-} = require("electron-devtools-installer");
+const isDev = require("electron-is-dev");
+const { default: installExtension, REDUX_DEVTOOLS } =
+  isDev && require("electron-devtools-installer");
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require("path");
 const url = require("url");
-const isDev = require("electron-is-dev");
 
 let mainWindow;
 
-installExtension(REDUX_DEVTOOLS)
-  .then(name => console.log(`Added Extension:  ${name}`))
-  .catch(err => console.log("An error occurred: ", err));
+if (isDev) {
+  installExtension(REDUX_DEVTOOLS)
+    .then(name => console.log(`Added Extension:  ${name}`))
+    .catch(err => console.log("An error occurred: ", err));
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({ show: false, autoHideMenuBar: true });
