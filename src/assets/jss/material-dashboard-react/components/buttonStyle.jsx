aa5684ab4c7bbe430b187b62e273ae26a8949604
Wout Heijnen <woutheijnen@hotmail.com>
2018-12-16 21:48:30 +0100

    Less forced css for custom buttons

--- src/assets/jss/material-dashboard-react/components/buttonStyle.jsx
+++ src/assets/jss/material-dashboard-react/components/buttonStyle.jsx
@@ -6,7 +6,7 @@ import {
   warningColor,
   dangerColor,
   roseColor
-} from "assets/jss/material-dashboard-react.jsx";
+} from "../../material-dashboard-react.jsx";
 
 const buttonStyle = {
   button: {
@@ -23,11 +23,10 @@ const buttonStyle = {
     margin: ".3125rem 1px",
     fontSize: "12px",
     fontWeight: "400",
-    textTransform: "uppercase",
+    textTransform: "none",
     letterSpacing: "0",
     willChange: "box-shadow, transform",
-    transition:
-      "box-shadow 0.2s cubic-bezier(0.4, 0, 1, 1), background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1)",
+    transition: "box-shadow 0.2s cubic-bezier(0.4, 0, 1, 1), background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1)",
     lineHeight: "1.42857143",
     textAlign: "center",
     whiteSpace: "nowrap",
@@ -92,11 +91,11 @@ const buttonStyle = {
   primary: {
     backgroundColor: primaryColor,
     boxShadow:
-      "0 2px 2px 0 rgba(156, 39, 176, 0.14), 0 3px 1px -2px rgba(156, 39, 176, 0.2), 0 1px 5px 0 rgba(156, 39, 176, 0.12)",
+      "0 2px 2px 0 rgba(38, 159, 176, 0.14), 0 3px 1px -2px rgba(38, 159, 176, 0.2), 0 1px 5px 0 rgba(38, 159, 176, 0.12)",
     "&:hover,&:focus": {
       backgroundColor: primaryColor,
       boxShadow:
-        "0 14px 26px -12px rgba(156, 39, 176, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(156, 39, 176, 0.2)"
+        "0 14px 26px -12px rgba(38, 159, 176, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(38, 159, 176, 0.2)"
     }
   },
   info: {
