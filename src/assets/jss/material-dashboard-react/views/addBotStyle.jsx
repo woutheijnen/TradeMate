c7202a981ad3533b4d4fe60f481ab2d14d6ff740
Wout Heijnen <woutheijnen@hotmail.com>
2018-12-17 01:02:08 +0100

    Better button labels for add bot view

--- src/assets/jss/material-dashboard-react/views/addBotStyle.jsx
+++ src/assets/jss/material-dashboard-react/views/addBotStyle.jsx
@@ -5,14 +5,18 @@ const addBotStyle = {
   botType: {
     boxShadow: "none !important",
     margin: "0 20px",
+    padding: 18,
     width: "calc(100% / 3 - 40px)"
   },
   buttonLabel: {
-    display: "block"
+    display: "block",
+    fontSize: 17,
+    fontWeight: 700
   },
   buttonCaption: {
     display: "block",
     fontSize: 11,
+    fontWeight: 400,
     textAlign: "left"
   }
 };
