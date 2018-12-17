3814595c31d11737b8c1d03dbaa7550b2a2d0566
Wout Heijnen <woutheijnen@hotmail.com>
2018-12-17 01:02:39 +0100

    Add onClick to buttons & fix propTypes typo for add bot view

--- src/views/AddBot/AddBot.jsx
+++ src/views/AddBot/AddBot.jsx
@@ -13,42 +13,68 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import addBotStyle from "../../assets/jss/material-dashboard-react/views/addBotStyle";
 
 class AddBot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      botType: 0
+    };
+  }
+
   render() {
     const { classes } = this.props;
+    const { botType } = this.state;
 
     return (
       <GridContainer>
         <GridItem xs={12} sm={12} md={12}>
-          <Button className={classes.botType} color="primary" size="lg" variant="contained">
+          <Button
+            className={classes.botType}
+            color={botType === 1 ? "info" : "primary"}
+            size="lg"
+            variant="contained"
+            onClick={e => this.setState({ botType: 1 })}
+          >
             <span className={classes.buttonLabel}>
               Trader bot
               <br />
               <span className={classes.buttonCaption}>
                 Increases your stack by trading.
                 <br />
-                You can have multiple of these.
+                You can have multiple trader bots.
               </span>
             </span>
           </Button>
-          <Button className={classes.botType} color="primary" size="lg" variant="contained">
+          <Button
+            className={classes.botType}
+            color={botType === 2 ? "info" : "primary"}
+            size="lg"
+            variant="contained"
+            onClick={e => this.setState({ botType: 2 })}
+          >
             <span className={classes.buttonLabel}>
               Download bot
               <br />
               <span className={classes.buttonCaption}>
                 Fetches price data and keeps it up-to-date.
                 <br />
-                You can use only one, required for trader and research bots.
+                Limited to one. Required for trader and research bots.
               </span>
             </span>
           </Button>
-          <Button className={classes.botType} color="primary" size="lg" variant="contained">
+          <Button
+            className={classes.botType}
+            color={botType === 3 ? "info" : "primary"}
+            size="lg"
+            variant="contained"
+            onClick={e => this.setState({ botType: 3 })}
+          >
             <span className={classes.buttonLabel}>
               Research bot
               <br />
               <span className={classes.buttonCaption}>
-                Tries new strategy to find a better one.
+                Tries new strategies to find a better one.
                 <br />
-                You can use only one, trader bot can connect to this.
+                Limited to one. Trader bot can connect to it.
               </span>
             </span>
           </Button>
@@ -58,7 +84,7 @@ class AddBot extends Component {
   }
 }
 
-AddBot.PropTypes = {
+AddBot.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
